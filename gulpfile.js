0559bf758d625bdea2ed6efcb578d1f8ab313482
nighcrawl <ange.chierchia@gmail.com>
2017-03-04 15:00:24 +0100

    ajoute une tâche gulp pour l'optimisation des images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 var gulp = require('gulp'),
 	browserSync = require('browser-sync').create(),
 	compass = require('gulp-compass'),
+	imagemin = require('gulp-imagemin')
 	cp = require('child_process');
+
+// Minifie les images contenues dans le dossier _source/assets/img
+gulp.task('images', function() {
+	return gulp.src('_source/assets/img/**/*.+(png|jpg|jpeg|gif|svg)')
+	.pipe(imagemin())
+	.pipe(gulp.dest('_deploy/assets/img'))
+	.pipe(browserSync.reload({ stream: true}));
+});
+
+// Minifie les images contenues dans le dossier _source/contents
+gulp.task('contents', function() {
+	return gulp.src('_source/contents/**/*.+(png|jpg|jpeg|gif|svg)')
+	.pipe(imagemin())
+	.pipe(gulp.dest('_deploy/contents'))
+	.pipe(browserSync.reload({ stream: true}));
+});
 
 // Compile les CSS dès qu'on détecte un changement dans les fichiers Sass
 gulp.task('compile', function() {
@@ -25,8 +42,10 @@ gulp.task('build', function() {
 // Sert le site avec browserSync
 gulp.task('serve', ['build'], function() {
 	browserSync.init({ server: { baseDir:'_deploy/' } });
+	gulp.watch('_source/contents/**/*', ['contents']);
+	gulp.watch('_source/assets/img/**/*', ['images']);
 	gulp.watch('_source/assets/sass/**/*.scss', ['compile']);
 	//gulp.watch('_deploy/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['contents', 'images', 'build', 'serve']);
